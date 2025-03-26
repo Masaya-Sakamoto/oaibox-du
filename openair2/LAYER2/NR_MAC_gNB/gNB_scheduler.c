saffanazyan07/oai_w-agf

Scheduling measurement gaps to provide sufficient time for the UE to re-tune its transceiver to the target carrier, complete the set of measurements and then re-tune its transceiver back to the original carrier.

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -214,6 +214,7 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frame, sub_frame_
     LOG_I(NR_MAC, "Frame.Slot %d.%d\n%s\n", frame, slot, stats_output);
   }
 
+  nr_measgap_scheduling(module_idP, frame, slot);
   nr_mac_update_timers(module_idP, frame, slot);
 
   // This schedules MIB

openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
@@ -739,7 +739,7 @@ typedef struct nr_mac_rrc_ul_if_s {
   initial_ul_rrc_message_transfer_func_t initial_ul_rrc_message_transfer;
 } nr_mac_rrc_ul_if_t;
 
-typedef enum interrupt_followup_action { FOLLOW_INSYNC, FOLLOW_INSYNC_RECONFIG, FOLLOW_OUTOFSYNC  } interrupt_followup_action_t;
+typedef enum interrupt_followup_action { FOLLOW_INSYNC, FOLLOW_INSYNC_RECONFIG, FOLLOW_OUTOFSYNC, MEAS_GAP } interrupt_followup_action_t;
 
 /*! \brief UE list used by gNB to order UEs/CC for scheduling*/
 typedef struct {

Masaya-Sakamoto/oaibox-du

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -210,6 +210,7 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP, frame_t frame, slot_t slo
     LOG_I(NR_MAC, "Frame.Slot %d.%d\n%s\n", frame, slot, stats_output);
   }
 
+  nr_measgap_scheduling(gNB, frame, slot);
   nr_mac_update_timers(module_idP, frame, slot);
 
   // This schedules MIB

openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
@@ -755,7 +755,7 @@ typedef struct nr_mac_rrc_ul_if_s {
   initial_ul_rrc_message_transfer_func_t initial_ul_rrc_message_transfer;
 } nr_mac_rrc_ul_if_t;
 
-typedef enum interrupt_followup_action { FOLLOW_INSYNC, FOLLOW_INSYNC_RECONFIG, FOLLOW_OUTOFSYNC  } interrupt_followup_action_t;
+typedef enum interrupt_followup_action { FOLLOW_INSYNC, FOLLOW_INSYNC_RECONFIG, FOLLOW_OUTOFSYNC, FOLLOW_ACTIVE } interrupt_followup_action_t;
 
 /*! \brief UE list used by gNB to order UEs/CC for scheduling*/
 typedef struct {