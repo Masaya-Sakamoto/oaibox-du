c8267ac4383751544d956974e40d6aed032f42c4
Luis Pereira <lpereira@allbesmart.pt>
2026-06-16 14:40:32 +0100

    Bump SIMDE commit to the most up-to-date (1c68d9a)
    Signed-off-by: Luis Pereira <lpereira@allbesmart.pt>

--- openair1/PHY/MODULATION/nr_modulation.c
+++ openair1/PHY/MODULATION/nr_modulation.c
@@ -7,8 +7,6 @@
 #include "PHY/NR_REFSIG/nr_mod_table.h"
 #include "executables/softmodem-common.h"
 #include <simde/x86/avx512.h>
-// Lacking declaration in present simde external package, will be detected as compilation error when they will add it
-#define simde_mm512_extracti64x2_epi64(a...) _mm512_extracti64x2_epi64(a)
 
 // #define DEBUG_DLSCH_PRECODING_PRINT_WITH_TRIVIAL // TODO: For debug, to be removed if want to merge to develop
 // #define DEBUG_LAYER_MAPPING
